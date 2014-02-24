9a4bf1de9a523a83350d9617785b50b2220f0236
sudosiyang <744276721@qq.com>
2014-02-24 17:49:26 +0800

    则加延迟加载

--- js/lib/base/util.js
+++ js/lib/base/util.js
@@ -21,68 +21,70 @@ define(function(require, exports) {
 	 ******************************/
 	var flag = true;
 	exports.validate = function() {
-		$("form").on("click", ".tooltip", function() {
-			$(this).prev().tooltip("destroy")
-		});
-		$('form input[type!=checkbox]:visible').focusout(function(event) {
-			if (!$(this).parent().attr("v")) {
-				//不为空
-				if (!$(this).val()) {
-					$(this).parent().tooltip({
-						"title": "输入的内容不能为空"
-					}).tooltip("show");
-					flag = false;
-					return;
-				}
-			}
-			//长短
-			if (!($(this).parent().attr("v") && !$(this).val().length)) {
-				var max = $(this).parent().attr('max') ? $(this).parent().attr('max') : 99999;
-				var min = $(this).parent().attr('min') ? $(this).parent().attr('min') : 0;
-				if (max < $(this).val().length || min > $(this).val().length) {
-					var title;
-					if (max == min) {
-						title = "请输入" + min + "个字符";
-					} else {
-						title = "请输入" + min + "~" + max + "个字符";
-					}
-					$(this).parent().tooltip({
-						"title": title
-					}).tooltip("show");
-					flag = false;
-					return;
-				}
-
-				//正则
-				if ($(this).parent().attr('reg')) {
-					var reg = new RegExp($(this).parent().attr('reg'), "g");
-					if (!$(this).val().match(reg)) {
+		require.async("../util/tooltip/tip", function() {
+			$("form").on("click", ".tooltip", function() {
+				$(this).prev().tooltip("destroy")
+			});
+			$('form input[type!=checkbox]:visible').focusout(function(event) {
+				if (!$(this).parent().attr("v")) {
+					//不为空
+					if (!$(this).val()) {
 						$(this).parent().tooltip({
-							"title": "您输入的内容不合法"
-						}).tooltip('show');
+							"title": "输入的内容不能为空"
+						}).tooltip("show");
 						flag = false;
 						return;
 					}
 				}
-			}
-		}).focusin(function(event) {
-			$(this).parent().tooltip("destroy");
-			flag = true;
-		});
+				//长短
+				if (!($(this).parent().attr("v") && !$(this).val().length)) {
+					var max = $(this).parent().attr('max') ? $(this).parent().attr('max') : 99999;
+					var min = $(this).parent().attr('min') ? $(this).parent().attr('min') : 0;
+					if (max < $(this).val().length || min > $(this).val().length) {
+						var title;
+						if (max == min) {
+							title = "请输入" + min + "个字符";
+						} else {
+							title = "请输入" + min + "~" + max + "个字符";
+						}
+						$(this).parent().tooltip({
+							"title": title
+						}).tooltip("show");
+						flag = false;
+						return;
+					}
 
-		$("form").submit(function(event) {
-			if (flag) {
-				$('input').focusout();
-				if ($(".tooltip").length > 0) {
-					return false;
-				} else {
-					$("input[type=submit]").val("正在提交...");
-					flag = false;
+					//正则
+					if ($(this).parent().attr('reg')) {
+						var reg = new RegExp($(this).parent().attr('reg'), "g");
+						if (!$(this).val().match(reg)) {
+							$(this).parent().tooltip({
+								"title": "您输入的内容不合法"
+							}).tooltip('show');
+							flag = false;
+							return;
+						}
+					}
 				}
-			} else {
-				return false;
-			};
-		});
+			}).focusin(function(event) {
+				$(this).parent().tooltip("destroy");
+				flag = true;
+			});
+
+			$("form").submit(function(event) {
+				if (flag) {
+					$('input').focusout();
+					if ($(".tooltip").length > 0) {
+						return false;
+					} else {
+						$("input[type=submit]").val("正在提交...");
+						flag = false;
+					}
+				} else {
+					return false;
+				};
+			});
+		})
 	}
 
 	/******************************
@@ -91,9 +93,9 @@ define(function(require, exports) {
 	exports.copy = function(btn, text_con, a_fn) {
 		//避免重复创建
 		if ($(btn).next().attr("class") == "zclip") return false;
-		
-		var path=require.resolve("../util/copy/copy");
-		var _path = path.split("js/")[0]+'js/res/copy.swf';
+
+		var path = require.resolve("../util/copy/copy");
+		var _path = path.split("js/")[0] + 'js/res/copy.swf';
 		//初始化
 		require.async("../util/copy/copy", function() {
 			$(btn).zclip({
@@ -119,39 +121,39 @@ define(function(require, exports) {
 	}
 
 	/**
-     * 解析模版tpl。当data未传入时返回编译结果函数；当某个template需要多次解析时，建议保存编译结果函数，然后调用此函数来得到结果。
-     * 
-     * @method parseTpl
-     * @grammar parseTpl(str, data)  ⇒ string
-     * @grammar parseTpl(str)  ⇒ Function
-     * @param {String} str 模板
-     * @param {Object} data 数据
-     * @example var str = "<p><%=name%></p>",
-     * obj = {name: 'ajean'};
-     * console.log($.parseTpl(str, data)); // => <p>ajean</p>
-     */
+	 * 解析模版tpl。当data未传入时返回编译结果函数；当某个template需要多次解析时，建议保存编译结果函数，然后调用此函数来得到结果。
+	 *
+	 * @method parseTpl
+	 * @grammar parseTpl(str, data)  ⇒ string
+	 * @grammar parseTpl(str)  ⇒ Function
+	 * @param {String} str 模板
+	 * @param {Object} data 数据
+	 * @example var str = "<p><%=name%></p>",
+	 * obj = {name: 'ajean'};
+	 * console.log($.parseTpl(str, data)); // => <p>ajean</p>
+	 */
 
-	exports.parseTpl = function( str, data ) {
-        var tmpl = 'var __p=[];' + 'with(obj||{}){__p.push(\'' +
-                str.replace( /\\/g, '\\\\' )
-                .replace( /'/g, '\\\'' )
-                .replace( /<%=([\s\S]+?)%>/g, function( match, code ) {
-                    return '\',' + code.replace( /\\'/, '\'' ) + ',\'';
-                } )
-                .replace( /<%([\s\S]+?)%>/g, function( match, code ) {
-                    return '\');' + code.replace( /\\'/, '\'' )
-                            .replace( /[\r\n\t]/g, ' ' ) + '__p.push(\'';
-                } )
-                .replace( /\r/g, '\\r' )
-                .replace( /\n/g, '\\n' )
-                .replace( /\t/g, '\\t' ) +
-                '\');}return __p.join("");',
+	exports.parseTpl = function(str, data) {
+		var tmpl = 'var __p=[];' + 'with(obj||{}){__p.push(\'' +
+			str.replace(/\\/g, '\\\\')
+			.replace(/'/g, '\\\'')
+			.replace(/<%=([\s\S]+?)%>/g, function(match, code) {
+				return '\',' + code.replace(/\\'/, '\'') + ',\'';
+			})
+			.replace(/<%([\s\S]+?)%>/g, function(match, code) {
+				return '\');' + code.replace(/\\'/, '\'')
+					.replace(/[\r\n\t]/g, ' ') + '__p.push(\'';
+			})
+			.replace(/\r/g, '\\r')
+			.replace(/\n/g, '\\n')
+			.replace(/\t/g, '\\t') +
+			'\');}return __p.join("");',
 
-            /* jsbint evil:true */
-            func = new Function( 'obj', tmpl );
-        
-        return data ? func( data ) : func;
-    };
+			/* jsbint evil:true */
+			func = new Function('obj', tmpl);
+
+		return data ? func(data) : func;
+	};
 
 
 
