64c6184f7cacf95f95330ffe2aab35d1a0c3321d
sudosiyang <744276721@qq.com>
2014-03-17 09:16:03 +0800

    add 卡片组件

---
+++ js/lib/util/card/card.js
@@ -0,0 +1,56 @@
+define(function(require, exports) {
+	require("../../../res/css/card.css");
+	var Tool = require("../../base/util");
+	var __target, __tip, __dataSource, __view = '<span class="__view" tabindex="-1"> <span class="__item">%%</span><b>▼</b>%%</span>';
+	var __ul = "",
+		__temp = "<li data-value='%%'>%%</li>";
+	var __onChange;
+
+	function init(option) {
+		__target = option.target;
+		__tip = option.tip;
+		__onChange = option.onChange;
+		createCard();
+	}
+
+	function createCard() {
+		__dataSource = $.parseJSON($(__target).attr('data-source'));
+		$.each(__dataSource, function(index, el) {
+			__ul += Tool.stringCat(__temp, el, index);
+		});
+		__ul = "<ul class='__clist'>" + __ul + "</ul>"
+		$(__target).append(Tool.stringCat(__view, __tip, __ul));
+		eventBind();
+	}
+
+	function eventBind() {
+		$(__target).on("click", "li", function(event) {
+			event.preventDefault();
+			var _value = $(this).attr('data-value'),
+				_text = $(this).text();
+			API.value = _value;
+			$(__target).find('.__view').attr("data-value", _value).find(".__item").text(_text).parent().find("li").removeClass();
+			$(this).addClass('__select').parent().slideUp();
+
+			__onChange.call(API);
+
+		}).on("click", ".__item,b", function() {
+			var _this = $(this).parent().find("ul");
+			_this.slideToggle(function() {
+				if (!_this.is(":hidden")) {
+					_this.parent().focus();
+				}
+			});
+		}).on("focusout",".__view" ,function() {
+			//失焦列表回弹
+			$(__target).find("ul").slideUp();
+		})
+	}
+	var API = (function() {
+		return {
+			value: ""
+		}
+	})();
+
+	exports.init = init;
+})
